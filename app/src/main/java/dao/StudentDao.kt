dec527368508670ce5698b8f0aef6a8fbeed1d5d
PODDAe <camalkaakash2@gmail.com>
2025-10-07 12:13:33 +0530

    Update StudentDao.kt

--- app/src/main/java/dao/StudentDao.kt
+++ app/src/main/java/dao/StudentDao.kt
@@ -1,1 +1,21 @@
+package com.nenapiyasa.fees.dao
+
+import androidx.room.*
+import com.nenapiyasa.fees.model.Student
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface StudentDao {
+    @Query("SELECT * FROM students")
+    fun getAll(): Flow<List<Student>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(student: Student)
+
+    @Update
+    suspend fun update(student: Student)
+
+    @Delete
+    suspend fun delete(student: Student)
+}
 
