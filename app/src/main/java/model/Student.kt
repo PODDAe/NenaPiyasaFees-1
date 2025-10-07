6f51cb4e001ac5153d2d7aaaa0672f1706f86db7
PODDAe <camalkaakash2@gmail.com>
2025-10-07 12:13:09 +0530

    Update Student.kt

--- app/src/main/java/model/Student.kt
+++ app/src/main/java/model/Student.kt
@@ -1,1 +1,13 @@
+package com.nenapiyasa.fees.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "students")
+data class Student(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val phone: String,
+    val isPaid: Boolean = false
+)
 
