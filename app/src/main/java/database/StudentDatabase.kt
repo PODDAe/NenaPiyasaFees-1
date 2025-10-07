6231f04c36fc58f69a256c0cbb417a6be6bf9a19
PODDAe <camalkaakash2@gmail.com>
2025-10-07 12:13:52 +0530

    Update StudentDatabase.kt

--- app/src/main/java/database/StudentDatabase.kt
+++ app/src/main/java/database/StudentDatabase.kt
@@ -1,1 +1,26 @@
+package com.nenapiyasa.fees.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.nenapiyasa.fees.dao.StudentDao
+import com.nenapiyasa.fees.model.Student
+
+@Database(entities = [Student::class], version = 1, exportSchema = false)
+abstract class StudentDatabase : RoomDatabase() {
+    abstract fun studentDao(): StudentDao
+
+    companion object {
+        @Volatile private var instance: StudentDatabase? = null
+
+        fun getDatabase(context: Context): StudentDatabase =
+            instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    StudentDatabase::class.java,
+                    "student_db"
+                ).build().also { instance = it }
+            }
+    }
+}
